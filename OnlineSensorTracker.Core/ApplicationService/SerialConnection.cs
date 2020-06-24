0edc21d1de622c8a4ca859460a18e132faa92e7a
AndreasAskjem <andaskjem@gmail.com>
2020-06-24 23:31:54 +0200

    This is not integrated  yet
    This code works on its own as a console application that print out values from the Serial, but needs modifications before it works with the rest of the program.

---
+++ OnlineSensorTracker.Core/ApplicationService/SerialConnection.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO.Ports;
+using System.Management;
+
+namespace OnlineSensorTracker.Core.ApplicationService
+{
+    internal class SerialConnection
+    {
+        public int Value { get; private set; } = 0;
+        public void SerialReader()
+        {
+            SerialPort port = new SerialPort();
+            port.PortName = GetArduinoPort();
+            port.BaudRate = 9600;
+            port.Parity = Parity.None;
+            port.DataBits = 8;
+            port.StopBits = StopBits.One;
+            port.Handshake = Handshake.None;
+            port.RtsEnable = true;
+
+            port.DataReceived += new SerialDataReceivedEventHandler(DataReceiver);
+
+            port.Open();
+            Console.ReadKey();
+            port.Close();
+        }
+
+        private void DataReceiver(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort myport = (SerialPort)sender;
+
+            string value = myport.ReadLine();
+
+            if (int.TryParse(value, out int intValue))
+            {
+                Value = intValue;
+            }
+        }
+
+
+        //Taken from https://stackoverflow.com/a/5397732/12216676
+        //It looks for connected Arduino devices and returns the name of the first it finds.
+        private string GetArduinoPort()
+        {
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
+
+            try
+            {
+                foreach (ManagementObject item in searcher.Get())
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    if (desc.Contains("Arduino"))
+                    {
+                        return deviceId;
+                    }
+                }
+            }
+            catch (ManagementException e)
+            {
+                /* Do Nothing */
+            }
+
+            return null;
+        }
+    }
+}
